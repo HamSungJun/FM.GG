96f293133b05a16d89177514d31f06dae03f46b7
HamSungJun <wleo12345@naver.com>
2019-10-28 21:23:27 +0900

    서버 스크립트

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname+'/build'));
+app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"build","index.html"));
+})
+
+app.listen(80, () => {
+    console.log('Example app listening on port 80!');
+})
